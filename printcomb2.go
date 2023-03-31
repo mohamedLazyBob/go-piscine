f6d484d20c29a653fa35c32db3e362b0c8ef0e0b
Mohamed Zaboub <45185441+mohamedLazyBob@users.noreply.github.com>
2023-03-31 08:06:32 +0000

    fix exersice

--- printcomb2.go
+++ printcomb2.go
@@ -1,25 +1,30 @@
-// Write a function that prints in ascending order and on a single line: all possible combinations of two different two-digit numbers.
-// These combinations are separated by a comma and a space.
-// without using the rune type, --allow-builtin
-// example 00 01, 00 02, 00 03, ..., 00 98, 00 99, 01 02, 01 03, ..., 97 98, 97 99, 98 99$
-
 package piscine
 
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := 0; i < 100; i++ {
-		for j := i + 1; j < 100; j++ {
-			if i != 0 || j != 1 {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					// ij > kl || ij == kl
+					if (i == k && j >= l) || i > k {
+						continue
+					}
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+					if i != '7' || j != '8' || k != '9' || l != '9' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
 			}
-			z01.PrintRune(rune(i/10 + 48))
-			z01.PrintRune(rune(i%10 + 48))
-			z01.PrintRune(' ')
-			z01.PrintRune(rune(j/10 + 48))
-			z01.PrintRune(rune(j%10 + 48))
 		}
 	}
+
 	z01.PrintRune('\n')
 }
