daf31a848379a8ed9a9992e6edf320f39915d03c
Mohamed Zaboub <45185441+mohamedLazyBob@users.noreply.github.com>
2023-04-03 06:32:12 +0000

    fix: some exercise

--- capitalize.go
+++ capitalize.go
@@ -12,19 +12,32 @@ func isAlphanumeric(s rune) bool {
 	return isLetter(s) || isDigit(s)
 }
 
+func toLower(s rune) rune {
+	if s >= 'A' && s <= 'Z' {
+		return s + 32
+	}
+	return s
+}
+func toUpper(s rune) rune {
+	if s >= 'a' && s <= 'z' {
+		return s - 32
+	}
+	return s
+}
+
 func Capitalize(s string) string {
 	runes := []rune(s)
 
-	if isLetter(runes[0]) {
-		runes[0] = runes[0] - 32
+	if IsLower(string(runes[0])) {
+		runes[0] = toUpper(runes[0])
 	}
 
 	for i := 1; i < len(runes); i++ {
 		if IsUpper(string(runes[i])) {
-			runes[i] = runes[i] + 32
+			runes[i] = toLower(runes[i])
 		}
 		if !isAlphanumeric(runes[i-1]) && IsLower(string(runes[i])) {
-			runes[i] = runes[i] - 32
+			runes[i] = toUpper(runes[i])
 		}
 	}
 	return string(runes)
