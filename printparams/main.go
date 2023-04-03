c80e832b78bd6c1252f1af1b8a182c64c443d491
Mohamed Zaboub <45185441+mohamedLazyBob@users.noreply.github.com>
2023-04-03 07:04:16 +0000

    fix: some exercise

---
+++ printparams/main.go
@@ -0,0 +1,21 @@
+// Write a program that prints the arguments received in the command line.
+
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStrln(s string) {
+	for i := 0; i < len(s); i++ {
+		z01.PrintRune(rune(s[i]))
+	}
+	z01.PrintRune('\n')
+}
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		printStrln(os.Args[i])
+	}
+}
