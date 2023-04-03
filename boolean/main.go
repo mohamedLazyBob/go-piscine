4132de8affa619b0198c3f825ab5ca48d922551b
Mohamed Zaboub <45185441+mohamedLazyBob@users.noreply.github.com>
2023-04-03 07:51:37 +0000

    fix: some exercise

---
+++ boolean/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr%2 == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+	lengthArgs := len(os.Args)
+	if isEven(lengthArgs) {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
